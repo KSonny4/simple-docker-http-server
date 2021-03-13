4c717e1dbd5d4fd165f324778b8dddda1630230f
Petr Kubelka <petr.kubelka@avast.com>
2021-03-13 19:23:15 +0100

    add parameters to test

--- tests/test_server.py
+++ tests/test_server.py
@@ -5,8 +5,18 @@ from web.web import WebServer
 
 
 @pytest.mark.asyncio
-async def test_web(web: WebServer) -> None:
-    url = f"http://{web._host}:{web._port}"
+@pytest.mark.parametrize(
+    "url_route",
+    [
+        "",
+        "index.html",
+        "text",
+        "test/test/" * 50,
+        "../../.."
+    ],
+)
+async def test_web(web: WebServer, url_route: str) -> None:
+    url = f"http://{web._host}:{web._port}/{url_route}"
 
     async with aiohttp.ClientSession() as session:
         async with session.get(url, raise_for_status=True) as resp:
