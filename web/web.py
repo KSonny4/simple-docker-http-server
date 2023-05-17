90a2089b5b1d69375a3d150133344b1c6d896c10
Petr Kubelka <petr.kubelka@ematiq.com>
2023-05-17 19:11:28 +0200

    use jsons

--- web/web.py
+++ web/web.py
@@ -2,9 +2,12 @@ import asyncio
 import logging
 import os
 from typing import Tuple
-
+import aiofiles
 from aiohttp import web
+from typing import Any, Dict
 
+from pathlib import Path
+import json
 logger = logging.getLogger(__name__)
 
 
@@ -18,22 +21,27 @@ class WebServer:
         self._port = port
         self._web_app = web.Application()
         self._runner = web.AppRunner(self._web_app)
+        self._response_json = None
+
+    async def get_json_response(self) -> Dict[Any, Any]:
+       # read file response.json using aiofiles
+       file_path = Path(os.path.dirname(__file__) , "response.json") 
+       async with aiofiles.open(file_path.resolve(), mode="r") as f:
+            return json.loads(await f.read())
 
     def _add_routes(self) -> None:
-        self._web_app.router.add_route("GET", "/{tail:.*}", self._get_html)
-        self._web_app.router.add_route("HEAD", "/{tail:.*}", self._get_html)
+        self._web_app.router.add_route("GET", "/{tail:.*}", self._get_json)
+        self._web_app.router.add_route("HEAD", "/{tail:.*}", self._get_json)
 
     async def start_web_server(self) -> None:
         self._add_routes()
         await self._runner.setup()
         site = web.TCPSite(self._runner, self._host, self._port)
+        self._response_json = await self.get_json_response()
         await site.start()
 
-    async def _get_html(self, _request: web.Request) -> web.Response:
-        return web.Response(
-            text=f"<p>This is html test web on {self._host}:{self._port}</p>",
-            content_type="text/html",
-        )
+    async def _get_json(self, _request: web.Request) -> web.Response:
+        return web.json_response(self._response_json)
 
     async def close(self) -> None:
         await self._runner.cleanup()
@@ -50,7 +58,7 @@ def setup() -> Tuple[str, int]:
         port = int(os.environ["PORT"])
         logger.info("Port is %s", port)
     except KeyError:
-        port = 8080
+        port = 8888
         logger.warning("Port was not provided via env. variable PORT, used %s", port)
     logger.info(f"Connect to http://%s:%s", host, port)
     return host, port
